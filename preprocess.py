86ac47c83209b739ba3fbf771c2afa481e01c24c
Hoyeon Jeong <recognizability@gmail.com>
2025-04-28 12:19:35 +0900

    Changed to a class

--- preprocess.py
+++ preprocess.py
@@ -23,141 +23,172 @@ from config import n_cores, seed, set_seed, cell_types_lung
 
 sc.settings.n_jobs = n_cores
 
-def prepare_sdata(path):
-    path_zarr = path + "data.zarr" 
-    if not os.path.exists(path_zarr):
-        sdata = xenium(
-            path=path,
-            n_jobs=n_cores,
-        )
-        print("Saving zarr ...")
-        sdata.write(path_zarr) 
-        print('Done.')
-    else:
-        print("Loading the zarr ...", end=' ')
-        sdata = sd.SpatialData.read(path_zarr)
-        print('done.')
-    return sdata
+class Preprocess():
+    def __init__(self, platform, sample):
+        self.platform = platform
+        self.sample = sample
+        self.pixel_size = json.load(open(f"/data0/{self.platform}/{self.sample}/experiment.xenium"))['pixel_size'] # micrometers per pixel
+        self.lower_bound = int(4 // self.pixel_size) #micrometer/(micrometer/pixel)
+        self.upper_bound = int(15 // self.pixel_size) #micrometer/(micrometer/pixel)
+        self.crop_size = self.upper_bound
 
-def inverse_affine_transform(x_pixel, y_pixel):
-    x_pixel = np.atleast_1d(x_pixel)
-    y_pixel = np.atleast_1d(y_pixel)
-    pixel_coords = np.stack([x_pixel, y_pixel], axis=1)
-    ones = np.ones((pixel_coords.shape[0], 1))
-    three_dimension = np.hstack([pixel_coords, ones]).T
-    inverse_affine = np.linalg.inv(affine)
-    transformed = inverse_affine @ three_dimension
-    x_transformed, y_transformed = transformed[:2]
-    if x_transformed.size == 1:
-        return x_transformed[0], y_transformed[0]
-    else:
-        return x_transformed, y_transformed
+        if 'lung' in self.sample or 'Lung' in self.sample:
+            self.organ = 'Lung'
+            self.cell_types = cell_types_lung
 
-def crop_he_image(cell_id):
-    centroid = cell_boundaries.loc[cell_id, "geometry"].centroid
-    xenium_x_um, xenium_y_um = centroid.x, centroid.y
-    xenium_x_px, xenium_y_px = xenium_x_um / pixel_size, xenium_y_um / pixel_size
-    he_x, he_y = inverse_affine_transform(xenium_x_px, xenium_y_px)
-    he_x, he_y = round(he_x), round(he_y)
-    half = crop_size // 2
-    x_min = int(he_x - half)
-    x_max = int(he_x + half)
-    y_min = int(he_y - half)
-    y_max = int(he_y + half)
+        raw_path = f"/data0/{self.platform}/{self.sample}/"
+        self.sdata = self._prepare_sdata(raw_path)
+        self.affine = get_transformation(self.sdata.images['he_image']).to_affine_matrix(input_axes=('x', 'y'), output_axes=('x', 'y'))
+        self.cell_boundaries = self.sdata.shapes["cell_boundaries"]
+        self.adata = self.sdata.tables['table']
 
-    image_path = processed_path + f'he{crop_size}/'
-    os.makedirs(image_path, exist_ok=True)
-    if 0 <= x_min and x_max < image_width and 0 <= y_min and y_max < image_height:
-        cropped_image = he_image_array[:, y_min:y_max, x_min:x_max]
-        cropped_image = cropped_image.transpose(1, 2, 0) # (c, y, x) to (y, x, c)
-        cropped_image = Image.fromarray(cropped_image)
-        cropped_image.save(image_path + f"{cell_id}.png")
-    
-def cell_area_filter():
-    bounds = sdata.shapes['cell_boundaries'].bounds
-    len_raw = len(bounds)
-    bounds = bounds / pixel_size
-    bounds['width'] = bounds.apply(lambda row: row['maxx'] - row['minx'], axis=1)
-    bounds['height'] = bounds.apply(lambda row: row['maxy'] - row['miny'], axis=1)
+        self.processed_path = args.directory + f"{self.platform}/{self.sample}/"
+        os.makedirs(self.processed_path, exist_ok=True)
 
-    fig, ax = plt.subplots(1,2)
-    sns.violinplot(bounds, x='width', ax=ax[0])
-    sns.violinplot(bounds, x='height', ax=ax[1])
-    fig.tight_layout()
-    fig.savefig(f"/data0/crp/results/violinplot_{args.platform}_{args.sample}.png", bbox_inches="tight")
-    plt.close(fig)
-    
-    filtered_cells = bounds[
-        (lower_bound <= bounds['width']) & 
-        (bounds['width'] < upper_bound) &
-        (lower_bound <= bounds['height']) & 
-        (bounds['height'] < upper_bound)
-    ].index
-    print(f"{len(filtered_cells)} cells are selected from {len_raw} cells.")
-    
-    return filtered_cells
+        self.he_image_array = self.sdata.images["he_image"]["scale0"]["image"].values 
+        self.image_channels, self.image_height, self.image_width = self.he_image_array.shape #(c, y, x)
 
-def crop_cells(cell_ids):
-    pool = Pool(n_cores)
-    expression_dfs = pool.map(crop_he_image, tqdm(cell_ids))
-    pool.close()
-    pool.join()
+        self.annotated_cell_ids = None
+        self.filtered_cell_ids = None
+        self.cell_ids = None
 
-def single_cell_reference():
-    if organ == 'Lung':
-        print("Loading LuCA single cell reference ... ", end='')
-        ref = sc.read_h5ad('/data0/cz_sc_reference/dd538ee7-f5e4-49e9-9f1e-2a1ea5246cf4.h5ad')
-        ref.index = ref.var.feature_name
-        ref.var.index = ref.var.feature_name
-        ref = ref[ref.obs['platform']!='Smart-seq2'].copy()
-        print(ref.shape)
-    return ref
+    def _prepare_sdata(self, path):
+        path_zarr = path + "data.zarr" 
+        if not os.path.exists(path_zarr):
+            sdata = xenium(
+                path=path,
+                n_jobs=n_cores,
+            )
+            print("Saving zarr ...")
+            sdata.write(path_zarr) 
+            print('Done.')
+        else:
+            print("Loading the zarr ...", end=' ')
+            sdata = sd.SpatialData.read(path_zarr)
+            print('done.')
+        return sdata
 
-def annotation(cell_subtype):
-    he_annotation = pd.read_csv(processed_path + f"annotation/merged_output.csv")
-    he_annotation = he_annotation.set_index("cell_id")[["group"]]
-    he_annotation.to_csv(processed_path + "annotation/he_annotation.csv")
-    he_annotation['Cell_type_HE'] = he_annotation['group'].astype(str)
-    annotated_cell_ids = he_annotation.index
-    
-    adata = sdata.tables['table']
-    sc_annotation_csv = processed_path + f'annotation/sc_annotation_{cell_subtype}.csv'
-    sc_annotation_h5ad = processed_path + f'annotation/sc_annotation_{cell_subtype}.h5ad'
-    if not (os.path.exists(sc_annotation_csv) and os.path.exists(sc_annotation_h5ad)) or args.force_annotate:
-        ref = single_cell_reference()
-        print('Annotating types of the cells ... ')
-        adata.obs[cell_subtype] = tc.tl.annotate(
-            adata,
-            ref,
-            annotation_key=cell_subtype,
-            assume_valid_counts=True,
-            remove_constant_genes=False,
-        ).T.idxmax()
-        adata.obs[cell_subtype].to_csv(sc_annotation_csv)
-        print(len(adata.obs[cell_subtype].unique()), 'subtypes are annotated.')
+    def single_cell_reference():
+        if self.organ == 'Lung':
+            print("Loading LuCA single cell reference ... ", end='')
+            ref = sc.read_h5ad('/data0/cz_sc_reference/dd538ee7-f5e4-49e9-9f1e-2a1ea5246cf4.h5ad')
+            ref.index = ref.var.feature_name
+            ref.var.index = ref.var.feature_name
+            ref = ref[ref.obs['platform']!='Smart-seq2'].copy()
+            print(ref.shape)
+        return ref
 
-        print('Setting neighbors for each cell ...')
-        sc.pp.neighbors(adata, random_state=seed)
-        print('Making UMAPs for each cell ...')
-        sc.tl.umap(adata, random_state=seed)
+    def annotation(self, cell_subtype='cell_type_tumor'):
+        he_annotation = pd.read_csv(self.processed_path + f"annotation/merged_output.csv")
+        he_annotation = he_annotation.set_index("cell_id")[["group"]]
+        he_annotation.to_csv(self.processed_path + "annotation/he_annotation.csv")
+        he_annotation['Cell_type_HE'] = he_annotation['group'].astype(str)
+        self.annotated_cell_ids = he_annotation.index
+        print(f"{len(self.annotated_cell_ids)} cells are annotated.")
+        
+        sc_annotation_csv = self.processed_path + f'annotation/sc_annotation_{cell_subtype}.csv'
+        sc_annotation_h5ad = self.processed_path + f'annotation/sc_annotation_{cell_subtype}.h5ad'
+        if not (os.path.exists(sc_annotation_csv) and os.path.exists(sc_annotation_h5ad)) or args.force_annotate:
+            ref = single_cell_reference()
+            print('Annotating types of the cells ... ')
+            self.adata.obs[cell_subtype] = tc.tl.annotate(
+                self.adata,
+                ref,
+                annotation_key=cell_subtype,
+                assume_valid_counts=True,
+                remove_constant_genes=False,
+            ).T.idxmax()
+            self.adata.obs[cell_subtype].to_csv(sc_annotation_csv)
+            print(len(self.adata.obs[cell_subtype].unique()), 'subtypes are annotated.')
 
-        adata.write_h5ad(sc_annotation_h5ad)
-    else: 
-        adata = sc.read_h5ad(sc_annotation_h5ad)
+            print('Setting neighbors for each cell ...')
+            sc.pp.neighbors(self.adata, random_state=seed)
+            print('Making UMAPs for each cell ...')
+            sc.tl.umap(self.adata, random_state=seed)
 
-        adata_file = processed_path + f'annotation/adata_he{crop_size}.h5ad'
-        if not os.path.exists(adata_file) or args.force_categorize:
-            adata.obs.index = adata.obs['cell_id']
-            adata.obs['Cell_subtype_ST'] = adata.obs[cell_subtype]
-            adata.obs['Cell_type_ST'] = adata.obs[cell_subtype].map({cell: group for group, subtypes in cell_types.items() for cell in subtypes})#.fillna('Other')
-            adata.obs = adata.obs.merge(he_annotation['Cell_type_HE'], how='left', left_index=True, right_index=True)
-            adata.obs['Cell_type_HE'] = adata.obs['Cell_type_HE']#.fillna('Other')
-            adata.obs['Cell_type'] = adata.obs.loc[adata.obs['Cell_type_ST'].astype(str) == adata.obs['Cell_type_HE'].astype(str), 'Cell_type_HE']
-            adata.obs = adata.obs[['Cell_subtype_ST', 'Cell_type_ST', 'Cell_type_HE', 'Cell_type']]
+            self.adata.write_h5ad(sc_annotation_h5ad)
+        else: 
+            self.adata = sc.read_h5ad(sc_annotation_h5ad)
 
-            adata.write(adata_file)
+            adata_file = self.processed_path + f'annotation/adata_he{self.crop_size}.h5ad'
+            if not os.path.exists(adata_file) or args.force_categorize:
+                self.adata.obs.index = self.adata.obs['cell_id']
+                self.adata.obs['Cell_subtype_ST'] = self.adata.obs[cell_subtype]
+                self.adata.obs['Cell_type_ST'] = self.adata.obs[cell_subtype].map({cell: group for group, subtypes in self.cell_types.items() for cell in subtypes})#.fillna('Other')
+                self.adata.obs = self.adata.obs.merge(he_annotation['Cell_type_HE'], how='left', left_index=True, right_index=True)
+                self.adata.obs['Cell_type_HE'] = self.adata.obs['Cell_type_HE']#.fillna('Other')
+                self.adata.obs['Cell_type'] = self.adata.obs.loc[self.adata.obs['Cell_type_ST'].astype(str) == self.adata.obs['Cell_type_HE'].astype(str), 'Cell_type_HE']
+                self.adata.obs = self.adata.obs[['Cell_subtype_ST', 'Cell_type_ST', 'Cell_type_HE', 'Cell_type']]
 
-    return annotated_cell_ids
+                self.adata.write(adata_file)
+            else:
+                self.adata = sc.read_h5ad(adata_file)
+
+    def inverse_affine_transform(self, x_pixel, y_pixel):
+        x_pixel = np.atleast_1d(x_pixel)
+        y_pixel = np.atleast_1d(y_pixel)
+        pixel_coords = np.stack([x_pixel, y_pixel], axis=1)
+        ones = np.ones((pixel_coords.shape[0], 1))
+        three_dimension = np.hstack([pixel_coords, ones]).T
+        inverse_affine = np.linalg.inv(self.affine)
+        transformed = inverse_affine @ three_dimension
+        x_transformed, y_transformed = transformed[:2]
+        if x_transformed.size == 1:
+            return x_transformed[0], y_transformed[0]
+        else:
+            return x_transformed, y_transformed
+
+    def _crop_he_image(self, cell_id):
+        centroid = self.cell_boundaries.centroid
+        xenium_x_um, xenium_y_um = centroid.x, centroid.y
+        xenium_x_px, xenium_y_px = xenium_x_um / self.pixel_size, xenium_y_um / self.pixel_size
+        he_x, he_y = inverse_affine_transform(xenium_x_px, xenium_y_px)
+        he_x, he_y = round(he_x), round(he_y)
+        half = self.crop_size // 2
+        x_min = int(he_x - half)
+        x_max = int(he_x + half)
+        y_min = int(he_y - half)
+        y_max = int(he_y + half)
+
+        image_path = self.processed_path + f'he{self.crop_size}/'
+        os.makedirs(image_path, exist_ok=True)
+        if 0 <= x_min and x_max < self.image_width and 0 <= y_min and y_max < self.image_height:
+            cropped_image = self.he_image_array[:, y_min:y_max, x_min:x_max]
+            cropped_image = cropped_image.transpose(1, 2, 0) # (c, y, x) to (y, x, c)
+            cropped_image = Image.fromarray(cropped_image)
+            cropped_image.save(image_path + f"{cell_id}.png")
+        
+    def cell_area_filter(self):
+        bounds = self.cell_boundaries.bounds
+        len_raw = len(bounds)
+        bounds = bounds / self.pixel_size
+        bounds['width'] = bounds.apply(lambda row: row['maxx'] - row['minx'], axis=1)
+        bounds['height'] = bounds.apply(lambda row: row['maxy'] - row['miny'], axis=1)
+        bounds = bounds.merge(self.adata.obs['Cell_type'], how='left', left_index=True, right_index=True)
+        bounds_melted = bounds.melt(id_vars='Cell_type', value_vars=['width', 'height'], var_name='Length', value_name='Value')
+
+
+        fig = plt.figure(figsize=(4, 4))
+        sns.violinplot(bounds_melted, x='Value', y='Cell_type', hue='Length', split=True)
+        fig.tight_layout()
+        fig.savefig(f"/data0/crp/results/violinplot_{self.platform}_{self.sample}.png", bbox_inches="tight")
+        plt.close(fig)
+        
+        self.filtered_cell_ids = bounds[
+            (self.lower_bound <= bounds['width']) & 
+            (bounds['width'] < self.upper_bound) &
+            (self.lower_bound <= bounds['height']) & 
+            (bounds['height'] < self.upper_bound)
+        ].index
+        print(f"{len(self.filtered_cell_ids)} cells are selected from {len_raw} cells.")
+
+    def crop_cells(self):
+        self.cell_ids = set(self.annotated_cell_ids) & set(self.filtered_cell_ids)
+        print(f'{len(self.cell_ids)} cells are prepared for model')
+        pool = Pool(n_cores)
+        expression_dfs = pool.map(self._crop_he_image, tqdm(self.cell_ids))
+        pool.close()
+        pool.join()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -171,28 +202,7 @@ if __name__ == "__main__":
     parser.add_argument("--force_categorize", action="store_true", help="If set, categorize again")
     args = parser.parse_args()
 
-    pixel_size = json.load(open(f"/data0/{args.platform}/{args.sample}/experiment.xenium"))['pixel_size'] # micrometers per pixel
-    lower_bound = int(4 // pixel_size) #micrometer/(micrometer/pixel)
-    upper_bound = int(15 // pixel_size) #micrometer/(micrometer/pixel)
-    crop_size = upper_bound
-
-    if 'lung' in args.sample or 'Lung' in args.sample:
-        organ = 'Lung'
-        cell_types = cell_types_lung
-
-    raw_path = f"/data0/{args.platform}/{args.sample}/"
-    sdata = prepare_sdata(raw_path)
-    affine = get_transformation(sdata.images['he_image']).to_affine_matrix(input_axes=('x', 'y'), output_axes=('x', 'y'))
-    cell_boundaries = sdata.shapes["cell_boundaries"]
-
-    processed_path = args.directory + f"{args.platform}/{args.sample}/"
-    os.makedirs(processed_path, exist_ok=True)
-
-    he_image = sdata.images["he_image"]["scale0"]
-    he_image_array = he_image["image"].values 
-    image_channels, image_height, image_width = he_image_array.shape #(c, y, x)
-
-    annotated_cell_ids = annotation('cell_type_tumor')
-    filtered_cell_ids = cell_area_filter()
-    cell_ids = set(annotated_cell_ids) & set(filtered_cell_ids)
-    crop_cells(cell_ids)
+    preprocess = Preprocess(args.platform, args.sample)
+    preprocess.annotation()
+    preprocess.cell_area_filter()
+    preprocess.crop_cells()
